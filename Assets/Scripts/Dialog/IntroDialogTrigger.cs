544ce582cb0468bd987bd80874458eeacb24774d
William <culturebunkerrecordings@gmail.com>
2021-03-22 18:11:39 +0000

    bat added + faun dialog for opening scene

---
+++ Assets/Scripts/Dialog/IntroDialogTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroDialogTrigger : MonoBehaviour
+{
+    DialogueTrigger dTrigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dTrigger = gameObject.GetComponent<DialogueTrigger>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            dTrigger.TriggerDialogue();
+        }
+    }
+}
