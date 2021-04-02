3a45bbba338e44ff4b2d54ff6dce8d2e39a7fb84
William <culturebunkerrecordings@gmail.com>
2021-04-03 00:09:12 +0100

    added shroom

---
+++ Assets/Scripts/Shrooms/ShroomController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShroomController : MonoBehaviour
+{
+    public float speed = 2f;
+    public bool facingRight = false;
+    public bool isMoving = false;
+    public Transform attackPoint;
+    public LayerMask playersLayer;
+    public float attackRange;
+    public bool attackingPlayer;
+
+    GameManager gManager;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        gManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gManager.gamePlaying)
+        {
+            attackCheck();
+        }
+    }
+
+    private void LateUpdate()
+    {
+        flip();
+    }
+
+
+    void flip()
+    {
+        Vector2 scale = transform.localScale;
+
+        if (facingRight && scale.x > 0 || !facingRight && scale.x < 0)
+        {
+            scale.x *= -1;
+        }
+
+        transform.localScale = scale;
+    }
+
+    void attackCheck()
+    {
+        attackingPlayer = Physics.CheckSphere(attackPoint.position, attackRange, playersLayer);
+    }
+
+}
---
+++ Assets/Scripts/Shrooms/ShroomNavMeshController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ShroomNavMeshController : MonoBehaviour
+{
+    GameObject player1;
+    GameObject player2;
+    ShroomController sController;
+    public bool isMoving;
+    GameManager gManager;
+
+    public NavMeshAgent nav;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sController = GetComponentInChildren<ShroomController>();
+        nav = GetComponent<NavMeshAgent>();
+        player1 = GameObject.Find("PlayerController");
+        player2 = GameObject.Find("Player2Controller");
+        gManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gManager.gamePlaying)
+        {
+            nav.SetDestination(player1.transform.position);
+
+            if (nav.pathEndPosition.x > nav.transform.position.x)
+            {
+                sController.facingRight = true;
+            }
+            else
+            {
+                sController.facingRight = false;
+            }
+
+            if (nav.velocity.magnitude > 0)
+            {
+                isMoving = true;
+            }
+            else
+            {
+                isMoving = false;
+            }
+        }
+    }
+}
